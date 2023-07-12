1845e6a76aa211bdda925d593adb4b1e00b73bee
Arn Mendoza <117653794+arn555@users.noreply.github.com>
2023-07-13 06:22:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+<main id="main-div" class="clear-fix">
+                
+    <!----------------  SIDEBAR ----------->
+    <sidebar id="sidebar-div" class="col-lg-3 sidebar">
+        
+        <button class="newest-women-btn">
+            Newest Women's Profiles
+        </button>
+
+        <button class="newest-women-btn">
+            Newest Ladies Worldwide
+        </button>
+
+        <button class="newest-women-btn">
+            Search for Women
+        </button>
+
+        <button class="newest-women-btn">
+            Global search Tool
+        </button>
+
+        <div class="next-tour-area"> 
+            All European tours will be on hold until further notice.
+        </div>
+
+        <div class="container sidebar-contact-number">
+            <img src="https://russia-ladies.com/invar/ssi-common/contact/contact-number.png" alt >
+        </div>
+        
+        <div class="sample">WOMEN'S PROFILES & SEARCH OPTION</div>
+
+        </div>
+
+        <ul >
+            <li>Men's FREE Registration</li>
+            <li>Russia Ladies Profiles</li>
+            <li>Ukraine Ladies Profile</li>
+            <li>Want More? Single Women Worldwide</li>
+            <li>This Weeks New Girls Worldwide</li>
+            <li>Advance Search</li>
+            <li>Weekly Auto Match World</li>
+        </ul>
+
+    </sidebar>
+
+    <!-- BODY AREA -->
+    <div id="bodyarea-div">
+        Body Area
+    </div>
+
+</main>
